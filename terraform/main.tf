806bd27d4dcee5ffb34029ed18e0c37dff1b3eab
Rafael Morel <rflmorel91@gmail.com>
2026-08-13 00:27:46 +0000

    Configure Terraform Proxmox provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,23 @@
 terraform {
   required_version = ">= 1.15.0"
+
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.90"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = "https://192.168.1.57:8006/"
+  api_token = var.proxmox_api_token
+  insecure  = true
+}
+
+variable "proxmox_api_token" {
+  type      = string
+  sensitive = true
 }
 
 output "terraform_version_check" {
