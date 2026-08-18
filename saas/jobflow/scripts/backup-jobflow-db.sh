25fa8c603fb536b093156d0de112a49e61ea23e6
Rafael Morel <rflmorel91@gmail.com>
2026-08-18 19:05:52 +0000

    Replicate JobFlow backups to OpenMediaVault

--- saas/jobflow/scripts/backup-jobflow-db.sh
+++ saas/jobflow/scripts/backup-jobflow-db.sh
@@ -8,6 +8,10 @@ BACKUP_DIR="$PROJECT_DIR/backups"
 TIMESTAMP="$(date '+%Y%m%d-%H%M%S')"
 BACKUP_FILE="$BACKUP_DIR/jobflow-$TIMESTAMP.dump"
 STATUS_FILE="$PROJECT_DIR/runtime/last-backup-success"
+OMV_HOST="192.168.1.137"
+OMV_USER="Rafael"
+OMV_KEY="$HOME/.ssh/jobflow_omv_backup"
+OMV_BACKUP_DIR="/srv/dev-disk-by-uuid-b3f81412-c245-4670-9fc1-1d0c80c74fe5/Data/jobflow-backups"
 
 mkdir -p "$BACKUP_DIR"
 
@@ -21,6 +25,18 @@ docker exec -i jobflow-db pg_restore \
   --list \
   < "$BACKUP_FILE" \
   > /dev/null
+scp \
+  -i "$OMV_KEY" \
+  -o IdentitiesOnly=yes \
+  -o BatchMode=yes \
+  "$BACKUP_FILE" \
+  "$OMV_USER@$OMV_HOST:$OMV_BACKUP_DIR/"
+ssh \
+  -i "$OMV_KEY" \
+  -o IdentitiesOnly=yes \
+  -o BatchMode=yes \
+  "$OMV_USER@$OMV_HOST" \
+  "find '$OMV_BACKUP_DIR' -type f -name 'jobflow-*.dump' -mtime +7 -delete"
 
 find "$BACKUP_DIR" \
   -type f \
