536e0c6808998a1d080aafc33f3803f1e00cafce
Rafael Morel <rflmorel91@gmail.com>
2026-08-20 18:35:16 +0000

    Remove JobFlow models from platform facade

--- saas/jobflow/backend/app/models/__init__.py
+++ saas/jobflow/backend/app/models/__init__.py
@@ -3,12 +3,6 @@ from app.models.admin_audit_log import AdminAuditLog
 from app.models.user import User
 from app.models.tenant_membership import TenantMembership
 from app.models.tenant import Tenant
-from app.products.jobflow.models.customer import Customer
-from app.products.jobflow.models.job import Job
-from app.products.jobflow.models.estimate import Estimate
-from app.products.jobflow.models.invoice import Invoice
-from app.products.jobflow.models.payment import Payment
-from app.products.jobflow.models.schedule import Schedule
 from app.models.lead import Lead
 
 __all__ = [
@@ -17,11 +11,5 @@ __all__ = [
     "TenantMembership",
     "User",
     "Tenant",
-    "Customer",
-    "Job",
-    "Estimate",
-    "Invoice",
-    "Payment",
-    "Schedule",
     "Lead",
 ]
